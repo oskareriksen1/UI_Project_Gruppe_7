23500f4df9a6199a28c08c1c61ac99deeebd850b
oskareriksen1 <oska1001@stud.kea.dk>
2024-03-07 13:12:04 +0100

    fjf

---
+++ src/main/java/dk/kea/ui_project_gruppe_7/UiProjectGruppe7Application.java
@@ -0,0 +1,13 @@
+package dk.kea.ui_project_gruppe_7;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UiProjectGruppe7Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UiProjectGruppe7Application.class, args);
+    }
+
+}
---
+++ src/test/java/dk/kea/ui_project_gruppe_7/UiProjectGruppe7ApplicationTests.java
@@ -0,0 +1,13 @@
+package dk.kea.ui_project_gruppe_7;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UiProjectGruppe7ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}

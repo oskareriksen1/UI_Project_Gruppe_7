49edfeb706cdfc25fcf52cd67f78be4c62cc31a6
Oskar Eriksen <oska1001@stud.kea.dk>
2024-03-14 13:38:08 +0100

    lidt forskelligt <3

--- src/main/java/dk/kea/ui_project_gruppe_7/controller/UIcontroller.java
+++ src/main/java/dk/kea/ui_project_gruppe_7/controller/UIcontroller.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class UIcontroller {
 
-    @GetMapping("/GrædOverSpildtMælk")
+    @GetMapping("/")
     public String visUIProject(){
         return "GrædOverSpildtMælk";
 
